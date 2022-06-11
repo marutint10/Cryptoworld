899dee315f90b8047319630969efa31394249c6b
Maruti Nandan Tiwari <marutint10@gmail.com>
2022-06-11 10:12:55 +0530

    first commit

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import millify from 'millify';
+import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+import HTMLReactParser from 'html-react-parser';
+
+import { useGetExchangesQuery } from '../services/cryptoApi';
+import Loader from './Loader';
+
+const { Text } = Typography;
+const { Panel } = Collapse;
+
+const Exchanges = () => {
+  const { data, isFetching } = useGetExchangesQuery();
+  const exchangesList = data?.data?.exchanges;
+ // Note: To access this endpoint you need premium plan
+  if (isFetching) return <Loader />;
+
+  return (
+    <>
+      <Row>
+        <Col span={6}>Exchanges</Col>
+        <Col span={6}>24h Trade Volume</Col>
+        <Col span={6}>Markets</Col>
+        <Col span={6}>Change</Col>
+      </Row>
+    </>
+  );
+};
+
+export default Exchanges;
